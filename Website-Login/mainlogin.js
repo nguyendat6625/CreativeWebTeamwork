fb7264f67959283a5a7659d99d56e1ba94e200a5
nguyentiendat <nguyendat060625@gmail.com>
2024-04-29 07:37:41 +0700

    thêm chức năng show-password

--- Website-Login/mainlogin.js
+++ Website-Login/mainlogin.js
@@ -1,2 +1,30 @@
+const showpass = document.getElementById("show-pass");
+const inputPass = document.getElementById("user");
+showpass.addEventListener("click",function(){
+if (showpass.className=="mdieyeoff"){
+showpass.classList.replace("mdieyeoff","mdieye");
+}else{
+showpass.classList.replace("mdieye","mdieyeoff");
+}
+if (inputPass.type =="password"){
+    inputPass.type = "text";
+}else{
+inputPass.type = "password"
+}
+})
+const showrepass = document.getElementById("show-repass");
+const inputrePass = document.getElementById("repassword");
+showrepass.addEventListener("click",function(){
+if (showrepass.className=="mdieyeoff"){
+showrepass.classList.replace("mdieyeoff","mdieye");
+}else{
+showrepass.classList.replace("mdieye","mdieyeoff");
+}
+if (inputrePass.type =="password"){
+    inputrePass.type = "text";
+}else{
+inputrePass.type = "password"
+}
+})
 
    
