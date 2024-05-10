20b62960319e50e439f4829538484f6a7e6be712
nguyendat6625 <148068249+nguyendat6625@users.noreply.github.com>
2024-05-10 23:07:33 +0700

    Update mainlogin.js

--- Website-Login/mainlogin.js
+++ Website-Login/mainlogin.js
@@ -1,5 +1,5 @@
 const showpass = document.getElementById("show-pass");
-const inputPass = document.getElementById("password");
+const inputPass = document.getElementById("pass");
 showpass.addEventListener("click",function(){
 if (showpass.className=="mdieyeoff"){
 showpass.classList.replace("mdieyeoff","mdieye");
